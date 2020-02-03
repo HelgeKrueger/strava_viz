6efd0de422aa430d77294e763e6284a31643f52c
Helge <helge.krueger@gmail.com>
2020-02-03 12:50:52 +0100

    Remove the api; Migrating it into the app

--- strava_viz/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .serializers import StravaActivitySerializer
-from .models import StravaActivity
-
-
-class StravaActivityViewSet(viewsets.ModelViewSet):
-    queryset = StravaActivity.objects.all().order_by('-datetime')
-    serializer_class = StravaActivitySerializer
