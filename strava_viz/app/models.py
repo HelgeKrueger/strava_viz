f699081ef7342e2caab81eb769b7dc5f1181c49c
Helge <helge.krueger@gmail.com>
2020-02-03 12:59:11 +0100

    Continue migration towards not having api project

--- strava_viz/app/models.py
+++ strava_viz/app/models.py
@@ -1,8 +1,27 @@
+from enum import Enum
+
 from django.db import models
 from django.contrib.auth.models import User
+from django_enum_choices.fields import EnumChoiceField
 
 
 class StravaConnectionInformation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     access_token = models.CharField(max_length=100)
     refresh_token = models.CharField(max_length=100)
+
+
+class ActivityType(Enum):
+    RUN = 'run'
+    RIDE = 'ride'
+
+
+class StravaActivity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=255)
+    distance_meter = models.FloatField()
+    moving_time = models.FloatField()
+    activity_id = models.BigIntegerField()
+    datetime = models.DateTimeField()
+    activity_type = EnumChoiceField(ActivityType, default=ActivityType.RUN)
--- strava_viz/settings.py
+++ strava_viz/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'strava_viz.api.apps.ApiConfig',
     'strava_viz.app',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'webpack_loader',
 ]
 
